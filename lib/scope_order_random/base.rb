d38f809bdd7d744c9f5f39cae41802cdf49a8b0f
jonathan <jonathan@dell.local>
2011-12-26 00:47:40 +0100

    Bug fix

--- lib/scope_order_random/base.rb
+++ lib/scope_order_random/base.rb
@@ -1,27 +1,27 @@
-module ActiveRecord; module Acts; end; end
-module ActiveRecord::Acts::ScopeOrderRandom
+# module ActiveRecord; module Acts; end; end
+# module ActiveRecord::Acts::ScopeOrderRandom
 
-  def self.included(klass)
-		klass.class_eval do
-			extend(ClassMethods)
-		end
-  end
+  # def self.included(klass)
+		# klass.class_eval do
+			# extend(ClassMethods)
+		# end
+  # end
 
-	module ClassMethods
+	# module ClassMethods
 		
-		if ActiveRecord::Base.connection_config[:adapter] == "sqlite3"
-			def order_random
-				order("RANDOM()")
-			end
-		else
-			#PostgreSQL or MySQL
-			def order_random
-				order("RAND()")
-			end
-		end
+		# if ActiveRecord::Base.connection_config[:adapter] == "sqlite3"
+			# def order_random
+				# order("RANDOM()")
+			# end
+		# else
+			PostgreSQL or MySQL
+			# def order_random
+				# order("RAND()")
+			# end
+		# end
 		
-  end
+  # end
 
-end
+# end
 
 # ActiveRecord::Base.send(:include, ActiveRecord::Acts::ScopeOrderRandom)
